4ff853956fbe1237e3d3fdb703f4d4051ed8784e
dagda1 <dagda1@scotalt.net>
2014-10-04 07:43:23 +0100

    levenshtein distance

--- src/levenshtein/core.clj
+++ src/levenshtein/core.clj
@@ -1,6 +1,23 @@
 (ns levenshtein.core)
 
-(defn levenshtein
-  "a string metric for measuring the difference between two sequences"
-  [left right]
-  1)
+(defn levenshtein [newStr oldStr]
+  "a Clojure levenshtein implementation using transient data structure"
+  (let [n (count newStr) m (count oldStr)]
+    (cond
+     (= 0 n) m
+     (= 0 m) n
+     :else
+     (let [prev-col (transient (vec (range (inc m)))) col (transient [])]
+       (dotimes [i n]
+         (assoc! col 0 (inc i))
+         (dotimes [j m]
+           (assoc! col (inc j)
+                   (min (inc (get col j))
+                        (inc (get prev-col (inc j)))
+                        (+ (get prev-col j) (if (= (get newStr i) (get oldStr j)) 0 1)))))
+         (dotimes [i (count prev-col)]
+           (assoc! prev-col i (get col i))))
+       (last (persistent! col))))))
+
+;(levenshtein "gambol" "gumbo")
+
--- test/levenshtein/core_test.clj
+++ test/levenshtein/core_test.clj
@@ -1,25 +1,22 @@
 (ns levenshtein.core-test
-  (:require [clojure.test :refer :all]
-            [levenshtein.core :refer :all]))
+  (:use [clojure.test])
+  (:use [levenshtein.core] :reload-all))
 
 (deftest test-levenshtein
-  (is (= 1 (levenshtein "abc" "abb"))))
+  (is (= 0 (levenshtein "abc" "abc")))
+  (is (= 1 (levenshtein "abc" "abb")))
+  (is (= 3 (levenshtein "Saturday" "Sunday")))
+  (is (= 3 (levenshtein "Sunday" "Saturday")))
+  (is (= 3 (levenshtein "kitten" "sitting"))))
 
-;; (deftest test-levenshtein
-;;   (is (= 0 (levenshtein "abc" "abc")))
-;;   (is (= 1 (levenshtein "abc" "abb")))
-;;   (is (= 3 (levenshtein "Saturday" "Sunday")))
-;;   (is (= 3 (levenshtein "Sunday" "Saturday")))
-;;   (is (= 3 (levenshtein "kitten" "sitting"))))
-
-;; (deftest levenshtein-distance-test
-;;   (are [a b d] (= (levenshtein-distance a b) d)
-;;        "aba" "aba" 0
-;;        "aba" "ada" 1
-;;        "abca" "aca" 1
-;;        "abma" "aca" 2
-;;        "kitten" "sitting" 3
-;;        "Saturday" "Sunday" 3
-;;        "gumbo" "gambol" 2
-;;        "nBP8GaFHVls2dI8h9aK1FWdRgevf43" "925BCPcYhT5hs8L9T3K2T5C7U3Lz5v" 28
-;;        [1 2 4] [1 2 4 3] 1))
+(deftest levenshtein-distance-test
+  (are [a b d] (= (levenshtein a b) d)
+       "aba" "aba" 0
+       "aba" "ada" 1
+       "abca" "aca" 1
+       "abma" "aca" 2
+       "kitten" "sitting" 3
+       "Saturday" "Sunday" 3
+       "gumbo" "gambol" 2
+       "nBP8GaFHVls2dI8h9aK1FWdRgevf43" "925BCPcYhT5hs8L9T3K2T5C7U3Lz5v" 28
+       [1 2 4] [1 2 4 3] 1))
